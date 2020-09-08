7a1c99130b4aa9fa4ab06a6d90a7e203390e9fb6
Wei Liang <yeewliang@yahoo.com>
2020-09-08 17:16:47 +0800

    Create changeImage.py
    In this section, you will write a Python script named changeImage.py to process the supplier images. You will be using the PIL library to update all images within ~/supplier-data/images directory to the following specifications:
    
        Size: Change image resolution from 3000x2000 to 600x400 pixel
        Format: Change image format from .TIFF to .JPEG

---
+++ changeImage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+curDir = "./supplier-data/images"
+
+# go through each file
+files = [f for f in os.listdir(curDir)]
+for file in files:
+  if file.endswith(".tiff"):
+    print(file)
+    im = Image.open(curDir+file)
+    new_im = im.resize((600,400)).convert("RGB")
+    new_im.save(curDir + "/" + file + ".jpeg")
