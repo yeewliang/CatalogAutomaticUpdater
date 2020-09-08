884f7dcc535f9aacbc8754ddba301e36787d71ad
Wei Liang <yeewliang@yahoo.com>
2020-09-08 17:26:36 +0800

    Update supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import requests
+import os
 
 # This example shows how a file can be uploaded using
 # The Python Requests module
 
 url = "http://localhost/upload/"
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+curDir = "./supplier-data/images"
+
+# go through each file
+files = [f for f in os.listdir(curDir)]
+for file in files:
+  if file.endswith(".jpeg"):
+    with open(file, 'rb') as opened:
+      r = requests.post(url, files={'file': opened})
